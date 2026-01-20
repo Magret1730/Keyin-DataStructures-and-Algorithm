aa0d9b5d82cdc5160843ede415efbddec5d100c6
Magret <belloabiodun17@gmail.com>
2026-01-20 18:10:14 -0330

    Solved assignment of the Movie Theatre problem

--- Class-Assignments/MovieTheatre/src/main/java/org/example/Main.java
+++ Class-Assignments/MovieTheatre/src/main/java/org/example/Main.java
@@ -1,17 +1,77 @@
 package org.example;
 
+import java.util.Scanner;
+
+//Simulate a movie theater that helps users to book and reserve seats.
+//reserve seats (if seat is taken, let them know it is taken and suggest an available seat)
+//cancel seats
+//retrieve initial seating charting
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+        System.out.println("Welcome to Magret's Movie Theatre Booking System!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        // Scanner object for user inputs
+        Scanner scanner = new Scanner(System.in);
+
+        // Allocate fixed 2D Array size for seating arrangement
+//        int[][] seatingArrangements = new int[10][10];
+        int[][] seatingArrangements = new int[3][2];
+
+        while (true) {
+            int availableSeats = 0;
+
+            System.out.println();
+            System.out.println("This chart represents the seating arrangement of the theater: ");
+
+            // Display Seating Chart
+            for (int[] rows : seatingArrangements) {
+                for (int seat : rows) {
+                    if (seat == 0) {
+                        availableSeats++;
+                    }
+                    System.out.print(seat + " ");
+                }
+                System.out.println();
+            }
+
+            System.out.println();
+            System.out.println("Kindly select a seat by providing the row and column number or enter -1 to exit");
+
+            // Get user input for row and column
+            System.out.println("Enter row number (0-9): ");
+            int row = scanner.nextInt();
+
+            System.out.println("Enter column number (0-9): ");
+            int column = scanner.nextInt();
+
+            // Exit condition
+            if (row == -1 || column == -1) {
+                System.out.println("Thank you for using Magret's Movie Theatre booking system. Goodbye!");
+                break;
+            }
+
+            // Validation for row and column input
+            if (row < 0 || row >= seatingArrangements.length || column < 0 || column >= seatingArrangements[0].length) {
+                System.out.println("Invalid row or column number. Please enter values between 0 and 9.");
+                continue;
+            }
+
+            // Check for if all seats are reserved
+            if (availableSeats == 0) {
+                System.out.println("All seats are reserved. Please try again later.");
+                break;
+            }
+
+            if (seatingArrangements[row][column] == 1) {
+                System.out.println("Sorry, this seat is already reserved. Please choose another seat with 0 value.");
+            } else {
+                // Reserve the seat
+                seatingArrangements[row][column] = 1;
+                System.out.println("Seat " + row + "," + column + " has been successfully reserved for you.");
+            }
         }
+        scanner.close();
     }
 }
