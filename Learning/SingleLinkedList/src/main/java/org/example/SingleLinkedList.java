4037e48bdc65aa1c9e2db59dbc6da5f042b55932
Magret <belloabiodun17@gmail.com>
2026-01-27 16:17:18 -0330

    Delete method on a linked list

--- Learning/SingleLinkedList/src/main/java/org/example/SingleLinkedList.java
+++ Learning/SingleLinkedList/src/main/java/org/example/SingleLinkedList.java
@@ -4,6 +4,7 @@ public class SingleLinkedList {
     public Node head;
     public Node tail;
     public int size;
+    public Node previous;
 
     public Node createSingleLinkedList (int nodeValue) {
         Node node = new Node();
@@ -52,11 +53,10 @@ public class SingleLinkedList {
             Node tempNode = head;
             for (int i = 0; i < size; i++){
                 System.out.print(tempNode.value);
-                if (i != size -1){
+                if (i != size - 1){
                     System.out.print(" -> ");
                 }
                 tempNode = tempNode.next;
-
             }
             System.out.println("\n");
 
@@ -78,7 +78,39 @@ public class SingleLinkedList {
             return false;
         }
     }
+
     //EXERCISE 2: Given a location Implement a Delete method.
+    public String deleteNode(int nodeLocation) {
+        // If head is null
+        if (head == null) {
+            return "Linked List is empty.";
+        }
 
+        // If the location given is less than size
+        if (nodeLocation < 0 || nodeLocation >= size) {
+            return "Location is invalid.";
+        }
 
+        // If location is the head
+        if (nodeLocation == 0) {
+            int deletedValue = head.value;
+            head = head.next;
+            size--;
+
+            return "Deleted Value " + deletedValue + " has been deleted in location " + nodeLocation;
+        }
+
+        // If location is in the middle or tail
+        Node temp = head;
+
+        for (int i = 0; i < nodeLocation - 1; i++) {
+            temp = temp.next;
+        }
+
+        int deletedValue = temp.next.value;
+        temp.next = temp.next.next;
+
+        size--;
+        return "Deleted Value " + deletedValue + " has been deleted in location " + nodeLocation;
+    }
 }
